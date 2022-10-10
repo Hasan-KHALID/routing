bd9be14617bb3ad1e7193c2793e6f83a5468a84b
Hasan-KHALID <khalidhasan842@gmail.com>
2022-10-10 22:03:01 +0600

    1st commit

---
+++ src/app/collectinons/collectinons.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CollectinonsRoutingModule } from './collectinons-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CollectinonsRoutingModule
+  ]
+})
+export class CollectinonsModule { }
---
+++ src/app/elemens/elemens.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ElemensRoutingModule } from './elemens-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ElemensRoutingModule
+  ]
+})
+export class ElemensModule { }
